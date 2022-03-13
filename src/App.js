f64a73c9182bf75ecdfc2c88f98862be011e8afa
Maksym <cheestick@gmail.com>
2022-03-13 20:37:55 +0200

    eslint test App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import paintings from './paintings.json';
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
       <Section title="Top of the week">
         <PaintingList items={paintings} />
       </Section>

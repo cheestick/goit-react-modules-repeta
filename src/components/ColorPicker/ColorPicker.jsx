e9b8481d2c289d3f0ed2cd2bdde0b09dd4b1e780
Maksym <cheestick@gmail.com>
2022-03-14 11:23:01 +0200

    css modules

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,13 +1,13 @@
-import styles from './ColorPicker.module.css';
+import s from './ColorPicker.module.css';
 
 function ColorPicker({ options }) {
   return (
-    <div className={styles.ColorPicker}>
-      <h2 className={styles.ColorPicker__title}>Color Picker</h2>
+    <div className={s.container}>
+      <h2 className={s.title}>Color Picker</h2>
       <div>
         {options.map(option => (
           <span
-            className={styles.ColorPicker__option}
+            className={s.option}
             style={{ backgroundColor: option.color }}
             key={option.color}
           ></span>

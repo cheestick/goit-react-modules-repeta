2eb94f844f4185ae6083e147eca5de1b07b64ebc
Maksym <cheestick@gmail.com>
2022-03-14 13:31:22 +0200

    alert component type prop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import PaintingList from './components/PaintingList';
 // import Section from './components/Section';
 import ColorPicker from './components/ColorPicker/ColorPicker';
+import Alert from './components/Alert/Alert';
 import 'modern-normalize/modern-normalize.css';
 // import paintings from './paintings.json';
 import colorPickerOptions from './colorPickerOptions';
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <div>
       <ColorPicker options={colorPickerOptions} />
+      <Alert text="ALERT! Check something wrong." />
       {/* <Section title="Top of the week">
         <PaintingList items={paintings} />
       </Section>
--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,13 +1,19 @@
 import s from './Alert.module.css';
+import PropTypes from 'prop-types';
 
-function Alert({ options }) {
+function Alert({ text, type }) {
   return (
     <div>
       <p role="alert" className={s.alert}>
-        Alert text
+        {text}
       </p>
     </div>
   );
 }
+
+Alert.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['error', 'warning', 'success']),
+};
 
 export default Alert;

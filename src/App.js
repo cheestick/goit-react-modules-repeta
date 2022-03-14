450917b2de354005fffd9a502fe5635ff6c1a562
Maksym <cheestick@gmail.com>
2022-03-14 20:35:49 +0200

    reusable customizable component Box

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 // import PaintingList from './components/PaintingList';
 // import Section from './components/Section';
-import ColorPicker from './components/ColorPicker/ColorPicker';
-import Alert from './components/Alert/Alert';
+// import ColorPicker from './components/ColorPicker/ColorPicker';
+// import Alert from './components/Alert/Alert';
 import Container from './components/Container/Container';
 import 'modern-normalize/modern-normalize.css';
+import Box from './components/Box/Box';
 // import paintings from './paintings.json';
-import colorPickerOptions from './colorPickerOptions';
+// import colorPickerOptions from './colorPickerOptions';
 
 export default function App() {
   return (
     <Container>
-      <ColorPicker options={colorPickerOptions} />
+      <Box
+        type="small"
+        classNames="big red"
+        styles={{
+          color: '#ffffff',
+          fontWeight: 900,
+          fontSize: 24,
+          margin: '5px auto',
+          borderRadius: '1em',
+        }}
+      />
+      <Box
+        type="medium"
+        classNames=""
+        styles={{ margin: '5px auto', borderRadius: '1em' }}
+      />
+      <Box
+        type="large"
+        classNames=""
+        styles={{
+          backgroundColor: '#aa3322',
+          color: '#fff',
+          margin: '5px auto',
+          fontWeight: 500,
+          fontSize: 18,
+          borderRadius: '1em',
+        }}
+      />
+      {/* <ColorPicker options={colorPickerOptions} />
       <Alert text="ALERT! Default type" />
       <Alert text="ALERT! Success type." type="success" />
       <Alert text="ALERT! Warning type." type="warning" />
-      <Alert text="ALERT! Error type." type="error" />
+      <Alert text="ALERT! Error type." type="error" /> */}
       {/* <Section title="Top of the week">
         <PaintingList items={paintings} />
       </Section>
